43dfe841abced2683f3fbdf27163a47b844fac52
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-08-11 18:46:06 +0530

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { NavBar } from "./Navbar/NavBar";
+export { NotesMenuBar } from "./NotesMenuBar/NotesMenuBar";
+export { SearchBar } from "./SearchBar/SearchBar";
+export { SideBar } from "./SideBar/SideBar";
