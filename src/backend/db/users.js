5866aeb9a8490fc99fd25bec9fbf5ddb092b1c74
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-08-11 18:46:01 +0530

    mockbee

---
+++ src/backend/db/users.js
@@ -0,0 +1,18 @@
+import { v4 as uuid } from "uuid";
+import { formatDate } from "../utils/authUtils";
+/**
+ * User Database can be added here.
+ * You can add default users of your wish with different attributes
+ * */
+
+export const users = [
+  {
+    _id: uuid(),
+    firstName: "Ashutosh",
+    lastName: "Mishra",
+    email: "ashutoshrm01@gmail.com",
+    password: "Ashupass1234",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+];
