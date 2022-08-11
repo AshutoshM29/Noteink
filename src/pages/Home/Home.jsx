68a74719341c27887ccccdcfac6c326aba5ff04d
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-08-11 18:52:51 +0530

    feat: Added Homepage

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+import { NavBar, Editor, NotesMenuBar, SearchBar, SideBar, FilterModal } from "../../components/index";
+import { useServices, useFilter, useAppTheme, useSearch, useAuth } from "../../context/index";
+import "./Home.css";
+
+const Home = () => {
+
+  return (
+    <div className="notes-align">
+      <NavBar />
+      <div className="sidebar-align">
+        <SideBar />
+      </div>
+      <div className="hero-img">
+        <SearchBar />
+      </div>
+      <h4 className="section-header">HOME</h4>
+      <div className="container-main">
+        <div className="container-note">
+          <div className="container-ntitle">
+            <h2 className="heading-note">Title</h2>
+            <div>
+              <i className= "material-icons icon-pin">
+                push_pin
+              </i>
+            </div>
+          </div>
+          <div className="notes-body">Note body</div>
+          <div className="container-label">
+            labels
+          </div>
+          <div className="notes-menu">
+            <div className="date-n-time">
+              26/06/2022
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    );
+};
+
+export { Home };
