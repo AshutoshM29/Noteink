9cd96b403962fdb92b26e8cf29908c433a774f0a
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-08-16 15:52:51 +0530

    Create dataReducer.js

---
+++ src/reducers/dataReducer.js
@@ -0,0 +1,29 @@
+const dataReducer = (state, {
+  type,
+  payload
+}) => {
+  switch (type) {
+    case "SET_NOTES":
+      return {
+        ...state, notes: payload
+      };
+    case "ARCHIVE_NOTES":
+      return {
+        ...state, archives: payload.archives, notes: payload.notes
+      };
+    case "TRASH_NOTES":
+      return {
+        ...state, trash: payload.trash, notes: payload.notes
+      };
+    case "SET_PINNED_NOTES":
+      return {
+        ...state, pinned: payload
+      };
+    default:
+      throw new Error("Invalid case");
+  }
+};
+
+export {
+  dataReducer
+};
