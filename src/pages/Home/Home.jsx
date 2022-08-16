db08d9577194d11779911faee0f5cc32677d87ec
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-08-16 15:51:18 +0530

    feat: added Home functionality

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,41 +1,134 @@
-import { NavBar, SearchBar, SideBar } from "../../components/index";
+import { useEffect } from "react";
+import { NavBar, Editor, NotesMenuBar, SearchBar, SideBar, FilterModal } from "../../components/index";
+import { useServices, useFilter, useAppTheme, useSearch, useAuth } from "../../context/index";
 import "./Home.css";
 
 const Home = () => {
+const { state, setShowEditorModal, showEditorModal, getNewNotes } =
+useServices();
+const { showFilterModal, setShowFilterModal, filterState, showFilterData } =
+useFilter();
+const { theme } = useAppTheme();
+const { search } = useSearch();
+const { isAuthorized } = useAuth();
 
-  return (
-    <div className="notes-align">
-      <NavBar />
+useEffect(() => {
+theme === "light"
+? (document.body.style.backgroundColor = "")
+: (document.body.style.backgroundColor = "var(--gray-dark)");
+}, [theme]);
+
+useEffect(() => {
+if (isAuthorized) {
+getNewNotes();
+}
+}, []);
+
+return (
+<div className="notes-align">
+  <NavBar />
+  <FilterModal showFilterModal={showFilterModal} onClosingFilterModal={()=> setShowFilterModal(false)}
+    />
+    <Editor showEditorModal={showEditorModal} onClosingEditorModal={()=> setShowEditorModal(false)}
+      />
       <div className="sidebar-align">
         <SideBar />
       </div>
+
       <div className="hero-img">
         <SearchBar />
       </div>
       <h4 className="section-header">HOME</h4>
+
       <div className="container-main">
-        <div className="container-note">
+        {showFilterData
+        ? filterState.filteredData &&
+        filterState.filteredData
+        ?.filter((item) => {
+        if (search === "") {
+        return item;
+        } else if (
+        item.title.toLowerCase().includes(search.toLowerCase())
+        ) {
+        return item;
+        }
+        })
+        .map((item) => {
+        return (
+        <div className="container-note" key={item._id} style={{ ...item, backgroundColor: item.bgColor }}>
           <div className="container-ntitle">
-            <h2 className="heading-note">Title</h2>
+            <h2 className="heading-note">{item.title}</h2>
             <div>
-              <i className= "material-icons icon-pin">
+              <i className={ item.pinnedNote ? "material-icons icon-pin" : "material-icons-outlined icon-pin" }>
                 push_pin
               </i>
             </div>
           </div>
-          <div className="notes-body">Note body</div>
-          <div className="container-label">
-            labels
+          <div className="notes-body" dangerouslySetInnerHTML={{ __html: item.body }} />
+          <div class="container-label">
+            {item.tags.map((label) => {
+            return (
+            <p className="custom-label">
+              {label}
+            </p>
+            );
+            })}
           </div>
           <div className="notes-menu">
             <div className="date-n-time">
-              26/06/2022
+              {`${new Date(item.createdTime).toLocaleString()}`}
             </div>
+            <div className="priority-tab">{item.priority}</div>
+            <NotesMenuBar notes={item} menutype={false} />
           </div>
         </div>
+        );
+        })
+        : state.notes &&
+        state.notes
+        .filter((item) => {
+        if (search === "") {
+        return item;
+        } else if (
+        item.title.toLowerCase().includes(search.toLowerCase())
+        ) {
+        return item;
+        }
+        })
+        .map((item) => {
+        return (
+        <div className="container-note" key={item._id} style={{ ...item, backgroundColor: item.bgColor }}>
+          <div className="container-ntitle">
+            <h2 className="heading-note">{item.title}</h2>
+            <div>
+              <i className={ item.pinnedNote ? "material-icons icon-pin" : "material-icons-outlined icon-pin" }>
+                push_pin
+              </i>
+            </div>
+          </div>
+          <div className="notes-body" dangerouslySetInnerHTML={{ __html: item.body }} />
+          <div class="container-label">
+            {item.tags.map((label) => {
+            return (
+            <p className="custom-label">
+              {label}
+            </p>
+            );
+            })}
+          </div>
+          <div className="notes-menu">
+            <div className="date-n-time">
+              {`${new Date(item.createdTime).toLocaleString()}`}
+            </div>
+            <div className="priority-tab">{item.priority}</div>
+            <NotesMenuBar notes={item} menutype={false} />
+          </div>
+        </div>
+        );
+        })}
       </div>
-    </div>
-    );
+</div>
+);
 };
 
 export { Home };
