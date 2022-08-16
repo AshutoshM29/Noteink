8a8b4976dbdf105d6579754e1ecc6e0e63cc72d3
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-08-16 15:59:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,70 @@
 import "./App.css";
 import Mockman from "mockman-js";
 import { Routes, Route } from "react-router-dom";
+import { RequiresAuth } from "./components";
 import {
-  Archive,
-  Home,
-  Trash,
-  Label,
-  Logout,
-  Login,
-  Signup,
   LandingPage,
+  Signup,
+  Login,
+  Home,
+  Label,
+  Archive,
+  Trash,
   Profile,
   NotFound,
+  Logout,
 } from "./pages";
+import { useAuth, useAppTheme } from "./context";
 
 function App() {
+  const { isAuthorized } = useAuth();
+  const { theme } = useAppTheme();
+
   return (
+    <div className={`App ${theme}`}>
+      { isAuthorized }
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/archive" element={<Archive />} />
-        <Route path="/trash" element={<Trash />} />
-        <Route path="/label" element={<Label />} />
+        <Route
+          path="/profile"
+          element={
+            <RequiresAuth>
+              <Profile />
+            </RequiresAuth>
+          }
+        />
+        <Route
+          path="/archive"
+          element={
+            <RequiresAuth>
+              <Archive />
+            </RequiresAuth>
+          }
+        />
+        <Route
+          path="/trash"
+          element={
+            <RequiresAuth>
+              <Trash />
+            </RequiresAuth>
+          }
+        />
+        <Route
+          path="/label"
+          element={
+            <RequiresAuth>
+              <Label />
+            </RequiresAuth>
+          }
+        />
         <Route path="/logout" element={<Logout />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/mockman-test" element={<Mockman />} />
       </Routes>
+    </div>
   );
 }
 
