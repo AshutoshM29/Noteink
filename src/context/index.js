347d858aa30d120e70b31f3a5a5d0ae62db1787e
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-08-16 15:47:12 +0530

    Create index.js

---
+++ src/context/index.js
@@ -0,0 +1,33 @@
+export {
+  useAuth,
+  AuthProvider,
+} from "../context/authContext/authenticationContext.jsx";
+export {
+  useLabel,
+  LabelProvider,
+} from "../context/labelContext/LabelContext.jsx";
+export {
+  useTheme,
+  NoteThemeProvider,
+} from "../context/noteThemeContext/noteThemeContext";
+export {
+  usePriority,
+  PriorityProvider,
+} from "../context/priorityContext/priorityContext.jsx";
+export {
+  useServices,
+  ServiceProvider,
+} from "../context/serviceContext/serviceContext.jsx";
+export {
+  useFilter,
+  FilterModalProvider,
+} from "../context/filterModalContext/filterModalComponent";
+export { usePin, PinProvider } from "../context/pinContext/pinContext";
+export {
+  useAppTheme,
+  AppThemeProvider,
+} from "../context/appThemeContext/appThemeContext";
+export {
+  useSearch,
+  SearchProvider,
+} from "../context/searchContext/searchContext";
