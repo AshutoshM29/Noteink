07be52f03711387b7e1b150417c22837f4618734
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-08-16 15:58:33 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
-export { Archive } from "../pages/Archive/Archive";
-export { Home } from "../pages/Home/Home.jsx";
-export { Label } from "../pages/Label/Label.jsx";
-export { NotFound } from "../pages/NotFound/NotFound.jsx";
-export { Trash } from "../pages/Trash/Trash.jsx";
-export { Login } from "../pages/Authentication/Login/Login.jsx";
-export { Logout } from "../pages/Authentication/Logout/Logout.jsx";
-export { Signup } from "../pages/Authentication/Signup/Signup.jsx";
-export { LandingPage } from "../pages/LandingPage/LandingPage.jsx";
-export { Profile } from "../pages/Profile/Profile.jsx";
+export { LandingPage } from "./LandingPage/LandingPage";
+export { Signup } from "./Authentication/Signup/Signup";
+export { Login } from "./Authentication/Login/Login";
+export { Home } from "./Home/Home";
+export { Label } from "./Label/Label";
+export { Archive } from "./Archive/Archive";
+export { Trash } from "./Trash/Trash";
+export { Profile } from "./Profile/Profile";
+export { NotFound } from "./NotFound/NotFound";
+export { Logout } from "./Authentication/Logout/Logout";
+
+
