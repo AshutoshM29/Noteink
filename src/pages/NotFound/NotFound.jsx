f4a2dd353b65767256d2b82ea6a322a8046a2d12
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-08-16 15:51:59 +0530

    update: not found page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useAppTheme } from "../../context";
 import "./NotFound.css";
 
 const NotFound = () => {
+  const { theme } = useAppTheme();
+
+  useEffect(() => {
+    theme === "light"
+      ? (document.body.style.backgroundColor = "")
+      : (document.body.style.backgroundColor = "var(--gray-dark)");
+  }, [theme]);
+
   return (
     <div className="page-container">
       <h1 className="page-heading">404</h1>
-      <h5 className="page-subheading">This page is missing</h5>
-      <Link to="/home" className="btn btn-link btn-secondary-outline">
+      <h5 className="page-subheading">Page not found</h5>
+      <Link to="/home" className="btn btn-nfound">
         Go back to home
       </Link>
     </div>
