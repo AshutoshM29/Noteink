e2196aaff195c756c3b0bd13926de17a2aceb1d5
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-08-16 15:32:14 +0530

    feat: added navbar func

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useAppTheme } from "../../context/index";
 import "../Navbar/NavBar.css";
 import heroIcon from "../../asset/heroIcon.png";
 
 const NavBar = () => {
+const { theme, setTheme } = useAppTheme();
+
+const toggleAppTheme = () => {
+setTheme((prev) => (prev === "light" ? "dark" : "light"));
+};
+
+useEffect(() => {
+localStorage.setItem("app-theme", theme);
+}, [theme]);
+
 return (
 <nav className="header">
   <header className="nav-bar">
@@ -11,13 +23,13 @@ return (
         <Link to="/">
         <img className="hero-logo" loading="eager" src={heroIcon} alt="Logo" />
         </Link>
-        <Link to="/" className="nav-hero-heading">
+        <Link to="/home" className="nav-hero-heading">
         Notein'<span className="text-invert">K</span>
         </Link>
       </div>
-      <button className="btn btn-secondary">
+      <button onClick={toggleAppTheme} className="btn btn-secondary">
         <i className="material-icons">
-        wb_sunny
+          {theme === "light" ? "wb_sunny" : "dark_mode"}
         </i>
       </button>
     </div>
